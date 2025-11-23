743956ba3331142aa0cdd899f8e8f39ed43bf0f2
Demestrine <210716279+Demmyawuor@users.noreply.github.com>
2025-11-24 02:18:50 +0300

    Move app to root directory and fix CSS import

--- nurse-judy-app/src/main.jsx
+++ nurse-judy-app/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './styles/globals.css'
+import './styles/global.css'  // Change from globals.css to global.css
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
